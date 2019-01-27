188d66774ea0d4e74b4d32565fedd72a8da792a8
Xigrux <xing.jingyuan@outlook.com>
2019-01-27 12:08:46 -0500

    npm installed jqeury package

--- routes/form.js
+++ routes/form.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const sgMail = require('@sendgrid/mail');
 
+
 const sendGrid = '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';
 
 sgMail.setApiKey(sendGrid);
@@ -14,18 +15,21 @@ sgMail.setApiKey(sendGrid);
 // });
 
 module.exports = router;
-
-router.post('/join', function(request, response){
-  console.log(request.body.user.name);
-  console.log(request.body.user.email);
+let emailAddress;
+router.post('/join', function(req, res){
+  emailAddress = req.body.email;
 });
 
-const msg = {
-  to: 'test@example.com',
-  from: 'test@example.com',
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
 
-sgMail.send(msg);
+$('#sendEmail').click(function(){
+  console.log('button clicked');
+  sgMail.send(msg);
+});
+
+
+const msg = {
+  to: emailAddress,
+  from: 'xing.jingyuan@outlook.com',
+  subject: 'Here is you team',
+  html: '<strong>Have fun at the hackathon</strong>',
+};
